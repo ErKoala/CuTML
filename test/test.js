4bddf45fdc36a1f5f0417fae488bf851800c52ea
Diego <cippoloz@gmail.com>
2013-04-11 15:27:13 +0200

    First commit

---
+++ test/test.js
@@ -0,0 +1,23 @@
+var expect  = require('expect.js')
+  , cheerio = require('cheerio')
+  , CuTML   = require('../');
+
+describe('#CuTML', function() {
+  it('should generate a string', function() {
+    var fin = CuTML('<a>Ehi, I love ya!</a>', {maxSize: 5, reminder: '...'});
+    expect(fin).to.be.a('string');
+  });
+
+  it('should cut the string inside the tags', function() {
+    var str = '<img src="http://www.sample.it/durr.png" /><a>Ehi, I love ya!</a><span>yess</span>';
+    var fin = CuTML(str, {maxSize: 10, reminder: '...'});
+    $ = cheerio.load(fin);
+    expect($('a').text()).to.have.length(10);
+  });
+
+  it('should leave the string equal', function() {
+    var str = '<a>Ehi, I love ya!</a>';
+    var fin = CuTML(str, {maxSize: 10, reminder: '...', saveTags: ['a']});
+    expect(fin).to.equal(str);
+  });
+});
